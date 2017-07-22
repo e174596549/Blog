84a240296344569ad1f8c98cce5bc183e696d51e
e174596549 <ly6819@hotmail.com>
2017-07-22 22:50:45 +0800

    增加跨域

--- appBlog.js
+++ appBlog.js
@@ -10,6 +10,14 @@ app.use(function( req, res, next) {
     next()
 });
 
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By",' 3.2.1')
+    if(req.method=="OPTIONS") res.send(200);/*让options请求快速返回*/
+    else  next();
+});
 // 配置静态文件目录
 app.use(express.static('static'))
 
